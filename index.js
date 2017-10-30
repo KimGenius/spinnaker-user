52135fed46b8b50e8de1081aa18795ba996588f7
KimGenius <youngjae1047@naver.com>
2017-10-30 10:27:12 +0900

    genie Commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/user', function (req, res) {
-  res.send('Hello User! Version 3');
+  res.send('Hello User! Version Genie');
 });
 
 app.get('/healthcheck', function (req, res) {
-  res.send('checking health... OK!');
+  res.send('checking health... OK! Genie');
 });
 
 app.listen(7001, function () {
-  console.log('Example app listening on port 7001!');
+  console.log('Example app listening on port 7001! Genie');
 });
