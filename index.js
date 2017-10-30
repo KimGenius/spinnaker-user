16002a034e07a1c2c50ac9b1399584deb6683cb9
KimGenius <youngjae1047@naver.com>
2017-10-30 14:49:32 +0900

    8080

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/healthcheck', function (req, res) {
   res.send('checking health... OK! Genie');
 });
 
-app.listen(7001, function () {
-  console.log('Example app listening on port 7001! Genie');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080! Genie');
 });
